1a5130414842fb35ca82612deb25831e89c7af24
macluiggy <luiggy.macias@imagineapps.co>
2023-04-27 21:20:05 -0500

    affds

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,29 +1,16 @@
-// Importamos el decorador 'Module' desde el paquete '@nestjs/common'
 import { Module } from '@nestjs/common';
-
-// Importamos los controladores y proveedores de la aplicación
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
-// Importamos el módulo 'ConfigModule' desde el paquete '@nestjs/config'
 import { ConfigModule } from '@nestjs/config';
-import { UserModule } from './user/user.module';
 
-// Definimos el módulo 'AppModule' utilizando el decorador 'Module'
 @Module({
-  // Importamos el módulo 'ConfigModule' para cargar las variables de entorno
   imports: [
     ConfigModule.forRoot({
-      envFilePath: ['.env.development'], // Archivo de variables de entorno a cargar
-      isGlobal: true, // Variables de entorno disponibles en toda la aplicación
+      envFilePath: ['.env.development'],
+      isGlobal: true,
     }),
-    UserModule,
   ],
-
-  // Agregamos los controladores y proveedores a este módulo
   controllers: [AppController],
   providers: [AppService],
 })
-
-// Exportamos el módulo 'AppModule' para ser utilizado en la aplicación
 export class AppModule {}
