fd6e5d2d7faddaacb0faa6c413ccb78ce1b520e0
macluiggy <luiggy.macias@imagineapps.co>
2023-05-02 20:16:23 -0500

    passenger controller

--- src/passenger/passenger.controller.ts
+++ src/passenger/passenger.controller.ts
@@ -1,7 +1,46 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { ClientProxySuperFlights } from 'src/common/proxy/client-proxy';
 import config from 'src/config';
+import { PassengerDTO } from './dto/passenger.dto';
+import { Observable } from 'rxjs';
+import { IPassenger } from 'src/common/interfaces/passenger.interface';
+import { PassengerMSG } from 'src/common/constants';
 
 const { apiVersion } = config;
 
 @Controller(`${apiVersion}/passenger`)
-export class PassengerController {}
+export class PassengerController {
+  constructor(private readonly clientProxy: ClientProxySuperFlights) {}
+  private _clientProxyPassenger = this.clientProxy.clientProxyPassengers();
+
+  @Post()
+  create(@Body() passenger: PassengerDTO): Observable<IPassenger> {
+    return this._clientProxyPassenger.send(PassengerMSG.CREATE, passenger);
+  }
+
+  @Get()
+  findAll(): Observable<IPassenger[]> {
+    return this._clientProxyPassenger.send(PassengerMSG.FIND_ALL, '');
+  }
+
+  @Get(':id')
+  findOne(@Param() { id }): Observable<IPassenger> {
+    return this._clientProxyPassenger.send(PassengerMSG.FIND_ONE, id);
+  }
+
+  @Put(':id')
+  update(
+    @Param() { id },
+    @Body() passenger: PassengerDTO,
+  ): Observable<IPassenger> {
+    return this._clientProxyPassenger.send(PassengerMSG.UPDATE, {
+      id,
+      passenger,
+    });
+  }
+
+  @Delete(':id')
+  delete(@Param() { id }): Observable<IPassenger> {
+    return this._clientProxyPassenger.send(PassengerMSG.DELETE, id);
+  }
+}
