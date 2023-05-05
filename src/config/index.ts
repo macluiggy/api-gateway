72d6c1de6b954ce8f36c79b8a3ae6fd0971c96e2
macluiggy <luiggy.macias@imagineapps.co>
2023-05-04 20:35:16 -0500

    v1

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,6 +1,6 @@
 const config = {
   // Archivo de variables de entorno a cargar
-  apiVersion: process.env.API_VERSION || 'api/v2',
+  apiVersion: process.env.API_VERSION || 'api/v1',
 };
 
 export default config;
